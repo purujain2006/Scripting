38829d1db614281e7e39fa35d3e6d77c5b6f5cc0
drpain365 <81000890+drpain365@users.noreply.github.com>
2021-11-13 03:02:17 -0800

    Update script.sh

--- script.sh
+++ script.sh
@@ -44,7 +44,6 @@ cat /home/$CUSER/Desktop/Scripting-main/Scripting-main/Configs/apt/sources.list 
 cat /home/$CUSER/Desktop/Scripting-main/Scripting-main/Configs/apt/sources.list.save > /etc/apt/sources.list.save
 cat /home/$CUSER/Desktop/Scripting-main/Scripting-main/Configs/apt/apt.conf.d/10periodic > /etc/apt/apt.conf.d/10periodic
 cat /home/$CUSER/Desktop/Scripting-main/Scripting-main/Configs/apt/apt.conf.d/20auto-upgrades > /etc/apt/apt.conf.d/20auto-upgrades
-cat /home/$CUSER/Desktop/Scripting-main/Scripting-main/Configs/apt/apt.conf.d/50unattended-upgrades > /etc/apt/apt.conf.d/50unattended-upgrades
 echo " "
 echo "Make sure your update settings are up to date"
 echo " "
@@ -362,7 +361,14 @@ sudo apt-get --purge autoremove ircd-hybrid -y > /dev/null 2>&1
 sudo apt-get --purge autoremove vino -y > /dev/null 2>&1
 sudo apt-get --purge autoremove Rdesktop -y > /dev/null 2>&1
 sudo apt-get --purge autoremove Vinagre -y > /dev/null 2>&1
+#------------------------------------------------------------------------------------------
 
+#update important packages
+#------------------------------------------------------------------------------------------
+sudo apt install gufw
+sudo apt install firefox
+sudo apt install nautilus
+sudo apt install linux-generic
 #------------------------------------------------------------------------------------------
 
 #Delete unwanted users
@@ -408,12 +414,44 @@ printf "\033[1;31mDeleting dangerous files...\033[0m\n"
   read yea
 #------------------------------------------------------------------------------------------
 
+#Configuring Configs
+#------------------------------------------------------------------------------------------
+cat /home/$CUSER/Desktop/Scripting-main/Scripting-main/Configs/sudoers > /etc/sudoers
+
+cat /home/$CUSER/Desktop/Scripting-main/Scripting-main/Configs/lightdm.conf > /etc/lightdm/lightdm.conf
+
+cat /home/$CUSER/Desktop/Scripting-main/Scripting-main/Configs/lightdm.conf > /usr/share/lightdm/lightdm.conf.d/50-unity-greeter.conf > /dev/null 2>&1
+
+cat /home/$CUSER/Desktop/Scripting-main/Scripting-main/Configs/login.defs > /etc/login.defs
+
+cat /home/$CUSER/Desktop/Scripting-main/Scripting-main/Configs/apache2.conf > /etc/apache2/apache2.conf
+
+cat /home/$CUSER/Desktop/Scripting-main/Scripting-main/Configs/rc.local > /etc/rc.local
+
+cat /home/$CUSER/Desktop/Scripting-main/Scripting-main/Configs/sysctl.conf > /etc/sysctl.conf
+
+sysctl -ep
+
+cat /home/$CUSER/Desktop/Scripting-main/Scripting-main/Configs/my.cnf > /etc/mysql/my.cnf
+
+
+cat /home/$CUSER/Desktop/Scripting-main/Scripting-main/Configs/sshd_config > /etc/ssh/sshd_config
+
+cat /home/$CUSER/Desktop/Scripting-main/Scripting-main/Configs/php.ini > /etc/php/7.2/apache2/php.ini
+
+cat /home/$CUSER/Desktop/Scripting-main/Scripting-main/Configs/vsftpd.conf > /etc/vsftpd.conf
+
+cat /home/$CUSER/Desktop/Scripting-main/Scripting-main/Configs/smb.conf > /etc/samba/smb.conf
+
+#------------------------------------------------------------------------------------------
+
 #Services
 #------------------------------------------------------------------------------------------
 sudo service ssh restart
 sudo service apache2 restart
 sudo service mysql restart
 sudo service samba restart
+sudo service smbd restart
 sudo service vsftpd restart
 sudo service proftpd restart
 sudo service ncftp restart
@@ -423,10 +461,118 @@ sudo service tftp restart
 printf "\033[1;31mCheck for suspicious services and disable them: \033[0m\n"
 echo " "
 service --status-all
+echo " "
+echo "type something"
+echo " "
+read k
 #------------------------------------------------------------------------------------------
 
 #Locks Root
 #------------------------------------------------------------------------------------------
 usermod -L root
+passwd -l root
 #------------------------------------------------------------------------------------------
 
+#Stuff that often breaks image :D
+#------------------------------------------------------------------------------------------
+sudo apt install libpam-cracklib
+
+cat /home/$CUSER/Desktop/Scripting-main/Scripting-main/Configs/common-password > /etc/pam.d/common-password
+
+cat /home/$CUSER/Desktop/Scripting-main/Scripting-main/Configs/common-auth > /etc/pam.d/common-auth
+
+
+sudo chmod 644 /etc/passwd
+sudo chmod 600 /etc/shadow
+
+
+sudo apt-get update && upgrade
+
+apt-get install -y iptables
+	#Clear out and default iptables
+	iptables -t nat -F
+	iptables -t mangle -F
+	iptables -t nat -X
+	iptables -t mangle -X
+	iptables -F
+	iptables -X
+	iptables -P INPUT DROP
+	iptables -P FORWARD DROP
+	iptables -P OUTPUT ACCEPT
+	ip6tables -t nat -F
+	ip6tables -t mangle -F
+	ip6tables -t nat -X
+	ip6tables -t mangle -X
+	ip6tables -F
+	ip6tables -X
+	ip6tables -P INPUT DROP
+	ip6tables -P FORWARD DROP
+	ip6tables -P OUTPUT DROP
+	#Block Bogons
+	printf "\033[1;31mEnter primary internet interface: \033[0m\n"
+	read interface
+	#Blocks bogons going into the computer
+	iptables -A INPUT -s 127.0.0.0/8 -i $interface -j DROP
+	iptables -A INPUT -s 0.0.0.0/8 -j DROP
+	iptables -A INPUT -s 100.64.0.0/10 -j DROP
+	iptables -A INPUT -s 169.254.0.0/16 -j DROP
+	iptables -A INPUT -s 192.0.0.0/24 -j DROP
+	iptables -A INPUT -s 192.0.2.0/24 -j DROP
+	iptables -A INPUT -s 198.18.0.0/15 -j DROP
+	iptables -A INPUT -s 198.51.100.0/24 -j DROP
+	iptables -A INPUT -s 203.0.113.0/24 -j DROP
+	iptables -A INPUT -s 224.0.0.0/3 -j DROP
+	#Blocks bogons from leaving the computer
+	iptables -A OUTPUT -d 127.0.0.0/8 -o $interface -j DROP
+	iptables -A OUTPUT -d 0.0.0.0/8 -j DROP
+	iptables -A OUTPUT -d 100.64.0.0/10 -j DROP
+	iptables -A OUTPUT -d 169.254.0.0/16 -j DROP
+	iptables -A OUTPUT -d 192.0.0.0/24 -j DROP
+	iptables -A OUTPUT -d 192.0.2.0/24 -j DROP
+	iptables -A OUTPUT -d 198.18.0.0/15 -j DROP
+	iptables -A OUTPUT -d 198.51.100.0/24 -j DROP
+	iptables -A OUTPUT -d 203.0.113.0/24 -j DROP
+	iptables -A OUTPUT -d 224.0.0.0/3 -j DROP
+	#Blocks outbound from source bogons - A bit overkill
+	iptables -A OUTPUT -s 127.0.0.0/8 -o $interface -j DROP
+	iptables -A OUTPUT -s 0.0.0.0/8 -j DROP
+	iptables -A OUTPUT -s 100.64.0.0/10 -j DROP
+	iptables -A OUTPUT -s 169.254.0.0/16 -j DROP
+	iptables -A OUTPUT -s 192.0.0.0/24 -j DROP
+	iptables -A OUTPUT -s 192.0.2.0/24 -j DROP
+	iptables -A OUTPUT -s 198.18.0.0/15 -j DROP
+	iptables -A OUTPUT -s 198.51.100.0/24 -j DROP
+	iptables -A OUTPUT -s 203.0.113.0/24 -j DROP
+	iptables -A OUTPUT -s 224.0.0.0/3 -j DROP
+	#Block receiving bogons intended for bogons - Super overkill
+	iptables -A INPUT -d 127.0.0.0/8 -i $interface -j DROP
+	iptables -A INPUT -d 0.0.0.0/8 -j DROP
+	iptables -A INPUT -d 100.64.0.0/10 -j DROP
+	iptables -A INPUT -d 169.254.0.0/16 -j DROP
+	iptables -A INPUT -d 192.0.0.0/24 -j DROP
+	iptables -A INPUT -d 192.0.2.0/24 -j DROP
+	iptables -A INPUT -d 198.18.0.0/15 -j DROP
+	iptables -A INPUT -d 198.51.100.0/24 -j DROP
+	iptables -A INPUT -d 203.0.113.0/24 -j DROP
+	iptables -A INPUT -d 224.0.0.0/3 -j DROP
+	iptables -A INPUT -i lo -j ACCEPT
+	#Least Strict Rules
+	#iptables -A INPUT -m conntrack --ctstate RELATED,ESTABLISHED -j ACCEPT
+	#Strict Rules -- Only allow well known ports (1-1022)
+	#iptables -A INPUT -p tcp --match multiport --sports 1:1022 -m conntrack --ctstate ESTABLISHED -j ACCEPT
+	#iptables -A INPUT -p udp --match multiport --sports 1:1022 -m conntrack --ctstate ESTABLISHED -j ACCEPT
+	#iptables -A OUTPUT -p tcp --match multiport --dports 1:1022 -m conntrack --ctstate NEW,ESTABLISHED -j ACCEPT
+	#iptables -A OUTPUT -p udp --match multiport --dports 1:1022 -m conntrack --ctstate NEW,ESTABLISHED -j ACCEPT
+	#iptables -A OUTPUT -o lo -j ACCEPT
+	#iptables -P OUTPUT DROP
+	#Very Strict Rules - Only allow HTTP/HTTPS, NTP and DNS
+	iptables -A INPUT -p tcp --sport 80 -m conntrack --ctstate ESTABLISHED -j ACCEPT
+	iptables -A INPUT -p tcp --sport 443 -m conntrack --ctstate ESTABLISHED -j ACCEPT
+	iptables -A INPUT -p tcp --sport 53 -m conntrack --ctstate ESTABLISHED -j ACCEPT
+	iptables -A INPUT -p udp --sport 53 -m conntrack --ctstate ESTABLISHED -j ACCEPT
+	iptables -A OUTPUT -p tcp --dport 80 -m conntrack --ctstate NEW,ESTABLISHED -j ACCEPT
+	iptables -A OUTPUT -p tcp --dport 443 -m conntrack --ctstate NEW,ESTABLISHED -j ACCEPT
+	iptables -A OUTPUT -p tcp --dport 53 -m conntrack --ctstate NEW,ESTABLISHED -j ACCEPT
+	iptables -A OUTPUT -p udp --dport 53 -m conntrack --ctstate NEW,ESTABLISHED -j ACCEPT
+	iptables -A OUTPUT -o lo -j ACCEPT
+	iptables -P OUTPUT DROP
