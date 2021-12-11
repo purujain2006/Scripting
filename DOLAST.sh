1f0dae54059be59ab0c0b7f46d553d8f82084129
drpain365 <81000890+drpain365@users.noreply.github.com>
2021-12-11 02:26:20 -0800

    Create DOLAST.sh

---
+++ DOLAST.sh
@@ -0,0 +1,160 @@
+function space () {
+    echo " "
+    echo " "
+}
+function red(){
+  printf "\033[31m$1\033[0m"
+}
+function green(){
+  printf "\033[32m$1\033[0m"
+}
+function yellow(){
+  printf "\033[33m$1\033[0m"
+}
+function blue(){
+  printf "\033[34m$1\033[0m"
+}
+function purple(){
+  printf "\033[35m$1\033[0m"
+}
+function dblue(){
+  printf "\033[36m$1\033[0m"
+}
+
+echo "User?"
+read CUSER
+
+sudo apt install libpam-cracklib
+
+rm -rf /etc/pam.d; cp -r /home/$CUSER/Desktop/Scripting-main/Scripting-main/Configs/pam.d /etc
+
+space
+space
+red "Make sure to change all critical service configs to 600 (file perms)"
+space
+sudo chmod 644 /etc/passwd
+sudo chmod 600 /etc/shadow
+chmod 0600 /etc/securetty
+chmod 700 /root
+chmod 600 /boot/grub/grub.cfg
+chmod 600 /etc/cron.allow
+chmod 750 /etc/apache2/conf* >/dev/null 2>&1
+chmod 511 /usr/sbin/apache2 >/dev/null 2>&1
+chmod 750 /var/log/apache2/ >/dev/null 2>&1
+chmod 640 /etc/apache2/conf-available/* >/dev/null 2>&1
+chmod 640 /etc/apache2/conf-enabled/* >/dev/null 2>&1
+chmod 640 /etc/apache2/apache2.conf >/dev/null 2>&1
+chmod og-rwx /boot/grub/grub.cfg
+chmod -R g-wx,o-rwx /var/log/*
+chown root:root /etc/ssh/sshd_config
+chmod og-rwx /etc/ssh/sshd_config
+chown root:root /etc/passwd
+chmod 644 /etc/passwd
+chown root:shadow /etc/shadow
+chmod o-rwx,g-wx /etc/shadow
+chown root:root /etc/group
+chmod 644 /etc/group
+chown root:shadow /etc/gshadow
+chmod o-rwx,g-rw /etc/gshadow
+chown root:root /etc/passwd-
+chmod 600 /etc/passwd-
+chown root:root /etc/shadow-
+chmod 600 /etc/shadow-
+chown root:root /etc/group-
+chmod 600 /etc/group-
+chown root:root /etc/gshadow-
+chmod 600 /etc/gshadow-
+df --local -P | awk {'if (NR!=1) print $6'} | xargs -I '{}' find '{}' -xdev -type d -perm -0002 2>/dev/null | xargs chmod a+t
+
+
+
+sudo apt-get update && upgrade
+
+apt-get install -y iptables
+	#Clear out and default iptables
+	iptables -t nat -F
+	iptables -t mangle -F
+	iptables -t nat -X
+	iptables -t mangle -X
+	iptables -F
+	iptables -X
+	iptables -P INPUT DROP
+	iptables -P FORWARD DROP
+	iptables -P OUTPUT ACCEPT
+	ip6tables -t nat -F
+	ip6tables -t mangle -F
+	ip6tables -t nat -X
+	ip6tables -t mangle -X
+	ip6tables -F
+	ip6tables -X
+	ip6tables -P INPUT DROP
+	ip6tables -P FORWARD DROP
+	ip6tables -P OUTPUT DROP
+	#Block Bogons
+	printf "\033[1;31mEnter primary internet interface: \033[0m\n"
+	read interface
+	#Blocks bogons going into the computer
+	iptables -A INPUT -s 127.0.0.0/8 -i $interface -j DROP
+	iptables -A INPUT -s 0.0.0.0/8 -j DROP
+	iptables -A INPUT -s 100.64.0.0/10 -j DROP
+	iptables -A INPUT -s 169.254.0.0/16 -j DROP
+	iptables -A INPUT -s 192.0.0.0/24 -j DROP
+	iptables -A INPUT -s 192.0.2.0/24 -j DROP
+	iptables -A INPUT -s 198.18.0.0/15 -j DROP
+	iptables -A INPUT -s 198.51.100.0/24 -j DROP
+	iptables -A INPUT -s 203.0.113.0/24 -j DROP
+	iptables -A INPUT -s 224.0.0.0/3 -j DROP
+	#Blocks bogons from leaving the computer
+	iptables -A OUTPUT -d 127.0.0.0/8 -o $interface -j DROP
+	iptables -A OUTPUT -d 0.0.0.0/8 -j DROP
+	iptables -A OUTPUT -d 100.64.0.0/10 -j DROP
+	iptables -A OUTPUT -d 169.254.0.0/16 -j DROP
+	iptables -A OUTPUT -d 192.0.0.0/24 -j DROP
+	iptables -A OUTPUT -d 192.0.2.0/24 -j DROP
+	iptables -A OUTPUT -d 198.18.0.0/15 -j DROP
+	iptables -A OUTPUT -d 198.51.100.0/24 -j DROP
+	iptables -A OUTPUT -d 203.0.113.0/24 -j DROP
+	iptables -A OUTPUT -d 224.0.0.0/3 -j DROP
+	#Blocks outbound from source bogons - A bit overkill
+	iptables -A OUTPUT -s 127.0.0.0/8 -o $interface -j DROP
+	iptables -A OUTPUT -s 0.0.0.0/8 -j DROP
+	iptables -A OUTPUT -s 100.64.0.0/10 -j DROP
+	iptables -A OUTPUT -s 169.254.0.0/16 -j DROP
+	iptables -A OUTPUT -s 192.0.0.0/24 -j DROP
+	iptables -A OUTPUT -s 192.0.2.0/24 -j DROP
+	iptables -A OUTPUT -s 198.18.0.0/15 -j DROP
+	iptables -A OUTPUT -s 198.51.100.0/24 -j DROP
+	iptables -A OUTPUT -s 203.0.113.0/24 -j DROP
+	iptables -A OUTPUT -s 224.0.0.0/3 -j DROP
+	#Block receiving bogons intended for bogons - Super overkill
+	iptables -A INPUT -d 127.0.0.0/8 -i $interface -j DROP
+	iptables -A INPUT -d 0.0.0.0/8 -j DROP
+	iptables -A INPUT -d 100.64.0.0/10 -j DROP
+	iptables -A INPUT -d 169.254.0.0/16 -j DROP
+	iptables -A INPUT -d 192.0.0.0/24 -j DROP
+	iptables -A INPUT -d 192.0.2.0/24 -j DROP
+	iptables -A INPUT -d 198.18.0.0/15 -j DROP
+	iptables -A INPUT -d 198.51.100.0/24 -j DROP
+	iptables -A INPUT -d 203.0.113.0/24 -j DROP
+	iptables -A INPUT -d 224.0.0.0/3 -j DROP
+	iptables -A INPUT -i lo -j ACCEPT
+	#Least Strict Rules
+	#iptables -A INPUT -m conntrack --ctstate RELATED,ESTABLISHED -j ACCEPT
+	#Strict Rules -- Only allow well known ports (1-1022)
+	#iptables -A INPUT -p tcp --match multiport --sports 1:1022 -m conntrack --ctstate ESTABLISHED -j ACCEPT
+	#iptables -A INPUT -p udp --match multiport --sports 1:1022 -m conntrack --ctstate ESTABLISHED -j ACCEPT
+	#iptables -A OUTPUT -p tcp --match multiport --dports 1:1022 -m conntrack --ctstate NEW,ESTABLISHED -j ACCEPT
+	#iptables -A OUTPUT -p udp --match multiport --dports 1:1022 -m conntrack --ctstate NEW,ESTABLISHED -j ACCEPT
+	#iptables -A OUTPUT -o lo -j ACCEPT
+	#iptables -P OUTPUT DROP
+	#Very Strict Rules - Only allow HTTP/HTTPS, NTP and DNS
+	iptables -A INPUT -p tcp --sport 80 -m conntrack --ctstate ESTABLISHED -j ACCEPT
+	iptables -A INPUT -p tcp --sport 443 -m conntrack --ctstate ESTABLISHED -j ACCEPT
+	iptables -A INPUT -p tcp --sport 53 -m conntrack --ctstate ESTABLISHED -j ACCEPT
+	iptables -A INPUT -p udp --sport 53 -m conntrack --ctstate ESTABLISHED -j ACCEPT
+	iptables -A OUTPUT -p tcp --dport 80 -m conntrack --ctstate NEW,ESTABLISHED -j ACCEPT
+	iptables -A OUTPUT -p tcp --dport 443 -m conntrack --ctstate NEW,ESTABLISHED -j ACCEPT
+	iptables -A OUTPUT -p tcp --dport 53 -m conntrack --ctstate NEW,ESTABLISHED -j ACCEPT
+	iptables -A OUTPUT -p udp --dport 53 -m conntrack --ctstate NEW,ESTABLISHED -j ACCEPT
+	iptables -A OUTPUT -o lo -j ACCEPT
+	iptables -P OUTPUT DROP
