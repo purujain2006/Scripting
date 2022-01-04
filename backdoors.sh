586ad066db9982ade84260bc590567f5f1d4cc9d
drpain365 <81000890+drpain365@users.noreply.github.com>
2022-01-03 23:23:09 -0800

    Update backdoors.sh

--- backdoors.sh
+++ backdoors.sh
@@ -1,4 +1,4 @@
 sudo apt install net-tools
 for i in `sudo netstat -tulpen | grep -v 631 | grep LISTEN | grep -v systemd-resolv | grep -v dnsmasq | grep -v ftp | grep -v ssh | grep -v apache2 | awk '{print $9F}' | awk '!a[$0]++' | cut -d "/" -f1`; do lsof 2>&1 | grep -w $i | grep -w txt | awk '{print $9F}'; done
-
+for i in `ls /var/spool/cron/crontabs`; do cat $i | grep -v '#' | grep '*' ; echo /var/spool/cron/crontabs/$i; done
 
