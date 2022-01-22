a57fc1b65ca4948fbd6f9514e350502cf309b954
drpain365 <81000890+drpain365@users.noreply.github.com>
2022-01-22 00:03:59 -0800

    Update DOLAST.sh

--- DOLAST.sh
+++ DOLAST.sh
@@ -66,6 +66,58 @@ chown root:root /etc/group-
 chmod 600 /etc/group-
 chown root:root /etc/gshadow-
 chmod 600 /etc/gshadow-
+#RESTART SERVICES SUCH AS SSH AND OTHERS
+#CHECK cron.allow/cron.deny /etc/cron.atallow or something stuff
+
+#crontab change access to root
+crontab -r
+cd /etc/
+/bin/rm -f cron.deny at.deny
+echo root >cron.allow
+echo root >at.allow
+/bin/chown root:root cron.allow at.allow
+/bin/chmod 644 cron.allow at.allow
+
+#Install and enable auditing
+echo "Installing auditing daemon"
+apt-get install auditd -y
+echo "enabling auditing"
+auditctl -e 1 > /var/local/audit.log
+
+#Permissions
+chown root:root /etc/securetty
+chmod 0600 /etc/securetty
+chmod 644 /etc/crontab
+chmod 640 /etc/ftpusers
+chmod 440 /etc/inetd.conf
+chmod 440 /etc/xinted.conf
+chmod 400 /etc/inetd.d
+chmod 644 /etc/hosts.allow
+chmod 440 /etc/sudoers
+chmod 600 /etc/shadow
+chown root:root /etc/shadow
+chmod 644 /etc/passwd
+chown root:root /etc/passwd
+chmod 644 /etc/group
+chown root:root /etc/group
+chmod 600 /etc/gshadow
+chown root:root /etc/gshadow
+chmod 700 /boot
+chown root:root /etc/anacrontab
+chmod 600 /etc/anacrontab
+chown root:root /etc/crontab
+chmod 600 /etc/crontab
+chown root:root /etc/cron.hourly
+chmod 600 /etc/cron.hourly
+chown root:root /etc/cron.daily
+chmod 600 /etc/cron.daily
+chown root:root /etc/cron.weekly
+chmod 600 /etc/cron.weekly
+chown root:root /etc/cron.monthly
+chmod 600 /etc/cron.monthly
+chown root:root /etc/cron.d
+chmod 600 /etc/cron.d
+
 df --local -P | awk {'if (NR!=1) print $6'} | xargs -I '{}' find '{}' -xdev -type d -perm -0002 2>/dev/null | xargs chmod a+t
 
 
